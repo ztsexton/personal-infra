db4a7097f1fa40b624894b36ca00fca4adec0481
Zach Sexton <zsexton2011@gmail.com>
2025-05-18 03:50:57 -0400

    Refactor Cloudflare DNS records: update TTL to standard values and disable proxying for zachsexton.com, petfoodfinder.app, and vigilo.dev; remove SSL settings for clarity and maintain DNSSEC configurations.

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -3,14 +3,14 @@ provider "cloudflare" {
   api_token = var.cloudflare_api_token
 }
 
-# Cloudflare DNS Records
+# Cloudflare DNS Records (DNS only, no proxy)
 resource "cloudflare_record" "zachsexton_root" {
   zone_id = var.cloudflare_zone_id_zachsexton
   name    = "@"
   content = hcloud_server.vps.ipv4_address
   type    = "A"
-  ttl     = 1  # Must be 1 when proxied is true
-  proxied = true  # Enable Cloudflare proxy
+  ttl     = 3600  # Standard TTL for DNS records
+  proxied = false  # Disable Cloudflare proxy - DNS only
 }
 
 resource "cloudflare_record" "petfoodfinder_root" {
@@ -18,8 +18,8 @@ resource "cloudflare_record" "petfoodfinder_root" {
   name    = "@"
   content = hcloud_server.vps.ipv4_address
   type    = "A"
-  ttl     = 1  # Must be 1 when proxied is true
-  proxied = true  # Enable Cloudflare proxy
+  ttl     = 3600  # Standard TTL for DNS records
+  proxied = false  # Disable Cloudflare proxy - DNS only
 }
 
 resource "cloudflare_record" "vigilo_root" {
@@ -27,57 +27,19 @@ resource "cloudflare_record" "vigilo_root" {
   name    = "@"
   content = hcloud_server.vps.ipv4_address
   type    = "A"
-  ttl     = 1  # Must be 1 when proxied is true
-  proxied = true  # Enable Cloudflare proxy
+  ttl     = 3600  # Standard TTL for DNS records
+  proxied = false  # Disable Cloudflare proxy - DNS only
 }
 
-# SSL settings for zachsexton.com - explicitly setting only what we need
-resource "cloudflare_zone_settings_override" "zachsexton_settings" {
-  zone_id = var.cloudflare_zone_id_zachsexton
-  
-  settings {
-    ssl = "strict"  # This sets "Full (strict)" SSL mode
-    always_use_https = "on"
-    automatic_https_rewrites = "on"
-    tls_1_3 = "on"
-  }
-}
-
-# SSL settings for petfoodfinder.app - explicitly setting only what we need
-resource "cloudflare_zone_settings_override" "petfoodfinder_settings" {
-  zone_id = var.cloudflare_zone_id_petfoodfinder
-  
-  settings {
-    ssl = "strict"  # This sets "Full (strict)" SSL mode
-    always_use_https = "on"
-    automatic_https_rewrites = "on"
-    tls_1_3 = "on"
-  }
-}
-
-# SSL settings for vigilo.dev - explicitly setting only what we need
-resource "cloudflare_zone_settings_override" "vigilo_settings" {
-  zone_id = var.cloudflare_zone_id_vigilo
-  
-  settings {
-    ssl = "strict"  # This sets "Full (strict)" SSL mode
-    always_use_https = "on"
-    automatic_https_rewrites = "on"
-    tls_1_3 = "on"
-  }
-}
-
-# DNSSEC settings for zachsexton.com
+# DNSSEC settings can still be maintained
 resource "cloudflare_zone_dnssec" "zachsexton_dnssec" {
   zone_id = var.cloudflare_zone_id_zachsexton
 }
 
-# DNSSEC settings for petfoodfinder.app
 resource "cloudflare_zone_dnssec" "petfoodfinder_dnssec" {
   zone_id = var.cloudflare_zone_id_petfoodfinder
 }
 
-# DNSSEC settings for vigilo.dev
 resource "cloudflare_zone_dnssec" "vigilo_dnssec" {
   zone_id = var.cloudflare_zone_id_vigilo
 }
