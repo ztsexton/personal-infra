0dcdfc0ae7f6a2f34c3499066fe757736c518b47
Zach Sexton <zsexton2011@gmail.com>
2025-09-09 23:19:38 -0400

    Refactor k3s and ArgoCD deployment: replace kubeconfig retrieval with direct ArgoCD installation via kubectl; remove unused Helm and Kubernetes provider configurations.

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -6,20 +6,4 @@ provider "cloudflare" {
   api_token = var.cloudflare_api_token
 }
 
-provider "helm" {
-  kubernetes {
-    host                   = "https://${hcloud_server.vps.ipv4_address}:6443"
-    client_certificate     = base64decode(local.kube_config.users[0].user.client-certificate-data)
-    client_key             = base64decode(local.kube_config.users[0].user.client-key-data)
-    cluster_ca_certificate = base64decode(local.kube_config.clusters[0].cluster.certificate-authority-data)
-  }
-}
-
-provider "kubernetes" {
-  host                   = "https://${hcloud_server.vps.ipv4_address}:6443"
-  client_certificate     = base64decode(local.kube_config.users[0].user.client-certificate-data)
-  client_key             = base64decode(local.kube_config.users[0].user.client-key-data)
-  cluster_ca_certificate = base64decode(local.kube_config.clusters[0].cluster.certificate-authority-data)
-}
-
 # Centralized provider definitions to keep external dependencies in one place.
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -8,21 +8,9 @@ terraform {
       source  = "cloudflare/cloudflare"
       version = "~> 4.0"
     }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.12"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.24"
-    }
     null = {
       source  = "hashicorp/null"
       version = "~> 3.2"
-    }
-    external = {
-      source  = "hashicorp/external"
-      version = "~> 2.3"
     }
   }
 }
